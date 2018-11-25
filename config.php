84244e728430021f3a91b9a5cdb55fc35daccec1
jeven2016 <jujucom@126.com>
2018-11-25 17:52:27 +0800

    add fastdfs and image service

---
+++ config.php
@@ -0,0 +1,48 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'instanceid' => 'ocj0ejoi7bx9',
+  'passwordsalt' => 'ltUhNEe/bnLD+rg2YoUe/Gokk56orl',
+  'secret' => 'XfR5mP7uNSZvTN0fRrsQQ9MWjDSmL7ukeUXJyWt5bkmcUdZ7',
+  'trusted_domains' => 
+  array (
+    0 => '119.3.66.114:5800',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '14.0.3.0',
+  'overwrite.cli.url' => 'http://119.3.66.114:5800',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'db_user',
+  'dbpassword' => 'Wzj_mysql01',
+  'installed' => true,
+  'mail_smtpmode' => 'smtp',
+  'mail_smtphost' => 'smtp.126.com',
+  'mail_from_address' => 'jujucom',
+  'mail_domain' => '126.com',
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtpport' => '25',
+  'mail_smtpauth' => 1,
+  'mail_smtpname' => 'jujucom',
+  'mail_smtppassword' => 'Wzj20080',
+);
